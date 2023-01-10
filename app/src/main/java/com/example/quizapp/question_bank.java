16813588fb5551f27abed408eb86b92e25aec058
surajprasad <suraj1737993@gmail.com>
2023-01-10 23:49:41 +0530

    layout changes

--- app/src/main/java/com/example/quizapp/question_bank.java
+++ app/src/main/java/com/example/quizapp/question_bank.java
@@ -8,12 +8,84 @@ public class question_bank {
     private static List<QuestionsList> Quiz_number1(){
         final  List<QuestionsList> questionsLists =new ArrayList<>();
 
-        final QuestionsList ques1 = new QuestionsList("This is dummuugy ques","option1","option2","option3","option4","option2",R.drawable.back_btn,"");
-        final QuestionsList ques2 = new QuestionsList("This is dummy ques","option1","option2","option3","option4","option2",R.drawable.algo_pic,"");
-        final QuestionsList ques3 = new QuestionsList("This is dummy ques","option1","option2","option3","option4","option4",R.drawable.algo_pic,"");
-        final QuestionsList ques4 = new QuestionsList("This is dummy ques","option1","option2","option3","option4","option3",R.drawable.ic_launcher_background,"");
-        final QuestionsList ques5 = new QuestionsList("This is dummy ques","option1","option2","option3","option4","option1",R.drawable.ic_launcher_background,"");
-        final QuestionsList ques6 = new QuestionsList("This is dummy ques","option1","option2","option3","option4","option2 ",R.drawable.ic_launcher_background,"");
+        final QuestionsList ques1 = new QuestionsList("This is dummy ques1 for quiz 1","option1","option2","option3","option4","option2",R.drawable.que1,"");
+        final QuestionsList ques2 = new QuestionsList("This is dummy que2 for quiz 1","option1","option2","option3","option4","option2",R.drawable.que2,"");
+        final QuestionsList ques3 = new QuestionsList("This is dummy que3 for quiz 1","option1","option2","option3","option4","option4",R.drawable.que3,"");
+        final QuestionsList ques4 = new QuestionsList("This is dummy que4 for quiz 1","option1","option2","option3","option4","option3",R.drawable.que4,"");
+        final QuestionsList ques5 = new QuestionsList("This is dummy que5 for quiz 1","option1","option2","option3","option4","option1",R.drawable.que5,"");
+        final QuestionsList ques6 = new QuestionsList("This is dummy que6 for quiz 1","option1","option2","option3","option4","option2 ",R.drawable.que6,"");
+
+
+        questionsLists.add(ques1);
+        questionsLists.add(ques2);
+        questionsLists.add(ques3);
+        questionsLists.add(ques4);
+        questionsLists.add(ques5);
+        questionsLists.add(ques6);
+
+
+        return questionsLists;
+
+
+    }
+
+    private static List<QuestionsList> Quiz_number2(){
+        final  List<QuestionsList> questionsLists =new ArrayList<>();
+
+        final QuestionsList ques1 = new QuestionsList("This is dummy ques1 for quiz 2","option1","option2","option3","option4","option2",R.drawable.que1,"");
+        final QuestionsList ques2 = new QuestionsList("This is dummy ques2 for quiz 2","option1","option2","option3","option4","option2",R.drawable.que2,"");
+        final QuestionsList ques3 = new QuestionsList("This is dummy que3 for quiz 2","option1","option2","option3","option4","option4",R.drawable.que3,"");
+        final QuestionsList ques4 = new QuestionsList("This is dummy ques4 for quiz 2","option1","option2","option3","option4","option3",R.drawable.que4,"");
+        final QuestionsList ques5 = new QuestionsList("This is dummy ques5 for quiz 2","option1","option2","option3","option4","option1",R.drawable.que5,"");
+        final QuestionsList ques6 = new QuestionsList("This is dummy ques6 for quiz 2","option1","option2","option3","option4","option2 ",R.drawable.que6,"");
+
+
+        questionsLists.add(ques1);
+        questionsLists.add(ques2);
+        questionsLists.add(ques3);
+        questionsLists.add(ques4);
+        questionsLists.add(ques5);
+        questionsLists.add(ques6);
+
+
+        return questionsLists;
+
+
+    }
+
+    private static List<QuestionsList> Quiz_number3(){
+        final  List<QuestionsList> questionsLists =new ArrayList<>();
+
+        final QuestionsList ques1 = new QuestionsList("This is dummy ques1 for quiz 3","option1","option2","option3","option4","option2",R.drawable.que1,"");
+        final QuestionsList ques2 = new QuestionsList("This is dummy ques2 for quiz 3","option1","option2","option3","option4","option2",R.drawable.que2,"");
+        final QuestionsList ques3 = new QuestionsList("This is dummy ques3 for quiz 3","option1","option2","option3","option4","option4",R.drawable.que3,"");
+        final QuestionsList ques4 = new QuestionsList("This is dummy ques4 for quiz 3","option1","option2","option3","option4","option3",R.drawable.que4,"");
+        final QuestionsList ques5 = new QuestionsList("This is dummy ques5 for quiz 3","option1","option2","option3","option4","option1",R.drawable.que5,"");
+        final QuestionsList ques6 = new QuestionsList("This is dummy ques6 for quiz 3","option1","option2","option3","option4","option2 ",R.drawable.que6,"");
+
+
+        questionsLists.add(ques1);
+        questionsLists.add(ques2);
+        questionsLists.add(ques3);
+        questionsLists.add(ques4);
+        questionsLists.add(ques5);
+        questionsLists.add(ques6);
+
+
+        return questionsLists;
+
+
+    }
+
+    private static List<QuestionsList> Quiz_number4(){
+        final  List<QuestionsList> questionsLists =new ArrayList<>();
+
+        final QuestionsList ques1 = new QuestionsList("This is dummy ques1 for quiz 4","option1","option2","option3","option4","option2",R.drawable.que1,"");
+        final QuestionsList ques2 = new QuestionsList("This is dummy ques2 for quiz 4","option1","option2","option3","option4","option2",R.drawable.que2,"");
+        final QuestionsList ques3 = new QuestionsList("This is dummy ques3 for quiz 4","option1","option2","option3","option4","option4",R.drawable.que3,"");
+        final QuestionsList ques4 = new QuestionsList("This is dummy ques4 for quiz 4","option1","option2","option3","option4","option3",R.drawable.que4,"");
+        final QuestionsList ques5 = new QuestionsList("This is dummy ques5 for quiz 4","option1","option2","option3","option4","option1",R.drawable.que5,"");
+        final QuestionsList ques6 = new QuestionsList("This is dummy ques6 for quiz 4","option1","option2","option3","option4","option2 ",R.drawable.que6,"");
 
 
         questionsLists.add(ques1);
@@ -34,6 +106,15 @@ public class question_bank {
         {
             case "quiz1":
                 return Quiz_number1();
+
+            case "quiz2":
+                return Quiz_number2();
+
+            case "quiz3":
+                return Quiz_number3();
+
+            case "quiz4":
+                return Quiz_number4();
 
             default:
                 return Quiz_number1();
--- app/src/main/java/com/example/quizapp/quiz_result.java
+++ app/src/main/java/com/example/quizapp/quiz_result.java
@@ -1,8 +1,12 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class quiz_result extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class quiz_result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_result);
+
+        final AppCompatButton strtNBtn=findViewById(R.id.end_btn);
+        final TextView correctAns=findViewById(R.id.correctAnswer);
+        final TextView incorrectAns=findViewById(R.id.incorrectAnswer);
+
+
+        final int getCorrectAnswers= getIntent().getIntExtra("correct",0);
+        final int getInCorrectAnswers= getIntent().getIntExtra("Incorrect",0);
+
+        correctAns.setText((String.valueOf(getCorrectAnswers)));
+        incorrectAns.setText((String.valueOf(getInCorrectAnswers)));
+
+        strtNBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(quiz_result.this,MainActivity.class));
+                finish();
+            }
+        });
+
+
+
     }
 }
